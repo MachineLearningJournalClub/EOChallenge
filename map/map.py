e0a6b518a91a88b6573352162f3aaa9660ffda32
beavillata <60182053+beavillata@users.noreply.github.com>
2021-07-03 19:41:42 +0200

    map

--- map/map.py
+++ map/map.py
@@ -12,72 +12,12 @@ m = folium.Map(location=[42.3601, -71.0589], zoom_start=3)
 
 states = os.path.join('data', 'countries.json')
 
-CH4_data = pd.read_csv(os.path.join('data', 'CH4.csv'))
-O3_data = pd.read_csv(os.path.join('data', 'O3.csv'))
-NO2_data = pd.read_csv(os.path.join('data', 'NO2.csv'))
-CO_data = pd.read_csv(os.path.join('data', 'CO.csv'))
-SO2_data = pd.read_csv(os.path.join('data', 'SO2.csv'))
+#CH4_data = pd.read_csv(os.path.join('data', 'CH4.csv'))
+#O3_data = pd.read_csv(os.path.join('data', 'O3.csv'))
+#NO2_data = pd.read_csv(os.path.join('data', 'NO2.csv'))
+#CO_data = pd.read_csv(os.path.join('data', 'CO.csv'))
+#SO2_data = pd.read_csv(os.path.join('data', 'SO2.csv'))
 index_data = pd.read_csv(os.path.join('data', 'seven.csv'))
-
-folium.Choropleth(
-    geo_data=states,
-    name='CH4',
-    data=CH4_data,
-    columns=['Country', 'CH4'],
-    key_on='feature.id',
-    fill_color='YlGn',
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name='Index CH4 [parts per billion]'
-).add_to(m)
-
-folium.Choropleth(
-    geo_data=states,
-    name='O3',
-    data=O3_data,
-    columns=['Country', 'O3'],
-    key_on='feature.id',
-    fill_color='PuRd',
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name='Index CH4 [mol/m^2]'
-).add_to(m)
-
-folium.Choropleth(
-    geo_data=states,
-    name='NO2',
-    data=NO2_data,
-    columns=['Country', 'NO2'],
-    key_on='feature.id',
-    fill_color='YlOrRd',
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name='Index CH4 [mol/m^2]'
-).add_to(m)
-
-folium.Choropleth(
-    geo_data=states,
-    name='CO',
-    data=CO_data,
-    columns=['Country', 'CO'],
-    key_on='feature.id',
-    fill_color='YlOrBr',
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name='Index CH4 [mol/m^2]'
-).add_to(m)
-
-folium.Choropleth(
-    geo_data=states,
-    name='SO2',
-    data=SO2_data,
-    columns=['Country', 'SO2'],
-    key_on='feature.id',
-    fill_color='YlGn',
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name='Index CH4 [mol/m^2]'
-).add_to(m)
 
 folium.Choropleth(
     geo_data=states,
@@ -97,9 +37,7 @@ png = 'img/ITA.png'
 encoded = base64.b64encode(open(png, 'rb').read())
 iframe = IFrame(html(encoded.decode('UTF-8')), width=1188, height=500)
 popup = folium.Popup(iframe, max_width=3000)
-icon = folium.Icon(color="blue", icon="ok")
-
-
+#icon = folium.Icon(color="blue", icon="ok")
 
 f=open(states)
 data=json.load(f)
@@ -108,17 +46,22 @@ onlyfiles.remove('.DS_Store')
 
 for file in onlyfiles:
     name=file.replace('.png','')
+    png = 'img/'+name+'.png'
+    encoded = base64.b64encode(open(png, 'rb').read())
+    iframe = IFrame(html(encoded.decode('UTF-8')), width=1188, height=500)
+    popup = folium.Popup(iframe, max_width=3000)
     for i in range(0, len(data['features'])):
         if data['features'][i]['id']==name:
+
             if len(data['features'][i]['geometry']['coordinates'])>1:
-                max_len=len(data['features'][i]['geometry']['coordinates'])-1
-                gj = folium.GeoJson(data={"type": "Polygon", "coordinates":data['features'][i]['geometry']['coordinates'][max_len]})
-                print(len(data['features'][i]['geometry']['coordinates']))
+                for a in range(0,len(data['features'][i]['geometry']['coordinates'])):
+                    gj = folium.GeoJson(data={"type": "Polygon", "coordinates":data['features'][i]['geometry']['coordinates'][a]})
+                    gj.add_child(folium.Popup(iframe, max_width=3000))
+                    gj.add_to(m)
             else:
                 gj = folium.GeoJson(data={"type": "Polygon", "coordinates":data['features'][i]['geometry']['coordinates']})
-                print(name)
-            gj.add_child(folium.Popup(iframe, max_width=3000))
-            gj.add_to(m)
+                gj.add_child(folium.Popup(iframe, max_width=3000))
+                gj.add_to(m)
 
 
 
